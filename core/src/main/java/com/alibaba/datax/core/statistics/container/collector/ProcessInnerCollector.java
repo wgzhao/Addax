c19ebf5ed6de9253fe2e527968ebc88c8410ff92
wgzhao <wgzhao@gmail.com>
2019-07-17 21:52:35 +0800

    merge code from huzt

--- core/src/main/java/com/alibaba/datax/core/statistics/container/collector/ProcessInnerCollector.java
+++ core/src/main/java/com/alibaba/datax/core/statistics/container/collector/ProcessInnerCollector.java
@@ -11,7 +11,7 @@ public class ProcessInnerCollector extends AbstractCollector {
 
     @Override
     public Communication collectFromTaskGroup() {
-        return LocalTGCommunicationManager.getJobCommunication();
+        return LocalTGCommunicationManager.getJobCommunication(this.getJobId());
     }
 
 }
