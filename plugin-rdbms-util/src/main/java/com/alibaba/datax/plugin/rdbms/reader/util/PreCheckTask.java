4ea34951847b1396bba4f72451d677235bc3c23b
wgzhao <wgzhao@gmail.com>
2020-08-10 21:02:27 +0800

    remove ADS plugin

--- plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/reader/util/PreCheckTask.java
+++ plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/reader/util/PreCheckTask.java
@@ -47,7 +47,7 @@ public class PreCheckTask implements Callable<Boolean>{
         Connection conn = DBUtil.getConnectionWithoutRetry(this.dataBaseType, jdbcUrl,
                 this.userName, password);
         int fetchSize = 1;
-        if(DataBaseType.MySql.equals(dataBaseType) || DataBaseType.DRDS.equals(dataBaseType)) {
+        if(DataBaseType.MySql.equals(dataBaseType) ) {
             fetchSize = Integer.MIN_VALUE;
         }
         try{
