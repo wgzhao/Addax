abe4c86441836158de238488a31152b9d531e4fa
wgzhao <wgzhao@gmail.com>
2020-09-08 17:29:07 +0800

    Remove hbase20xreader module, it needs more testing

--- core/src/main/java/com/alibaba/datax/core/container/util/HookInvoker.java
+++ core/src/main/java/com/alibaba/datax/core/container/util/HookInvoker.java
@@ -1,18 +1,11 @@
 package com.alibaba.datax.core.container.util;
 
-import com.alibaba.datax.common.exception.CommonErrorCode;
-import com.alibaba.datax.common.exception.DataXException;
-import com.alibaba.datax.common.spi.Hook;
 import com.alibaba.datax.common.util.Configuration;
-import com.alibaba.datax.core.util.FrameworkErrorCode;
-import com.alibaba.datax.core.util.container.JarLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.ServiceLoader;
 
 /**
  * 扫描给定目录的所有一级子目录，每个子目录当作一个Hook的目录。
@@ -33,43 +26,43 @@ public class HookInvoker {
         this.msg = msg;
     }
 
-    public void invokeAll() {
-        if (!baseDir.exists() || baseDir.isFile()) {
-            LOG.info("No hook invoked, because base dir not exists or is a file: " + baseDir.getAbsolutePath());
-            return;
-        }
+//    public void invokeAll() {
+//        if (!baseDir.exists() || baseDir.isFile()) {
+//            LOG.info("No hook invoked, because base dir not exists or is a file: " + baseDir.getAbsolutePath());
+//            return;
+//        }
+//
+//        String[] subDirs = baseDir.list((dir, name) -> new File(dir, name).isDirectory());
+//
+//        if (subDirs == null) {
+//            throw DataXException.asDataXException(FrameworkErrorCode.HOOK_LOAD_ERROR, "获取HOOK子目录返回null");
+//        }
+//
+//        for (String subDir : subDirs) {
+//            doInvoke(new File(baseDir, subDir).getAbsolutePath());
+//        }
+//
+//    }
 
-        String[] subDirs = baseDir.list((dir, name) -> new File(dir, name).isDirectory());
-
-        if (subDirs == null) {
-            throw DataXException.asDataXException(FrameworkErrorCode.HOOK_LOAD_ERROR, "获取HOOK子目录返回null");
-        }
-
-        for (String subDir : subDirs) {
-            doInvoke(new File(baseDir, subDir).getAbsolutePath());
-        }
-
-    }
-
-    private void doInvoke(String path) {
-        ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
-        try {
-            JarLoader jarLoader = new JarLoader(new String[]{path});
-            Thread.currentThread().setContextClassLoader(jarLoader);
-            Iterator<Hook> hookIt = ServiceLoader.load(Hook.class).iterator();
-            if (!hookIt.hasNext()) {
-                LOG.warn("No hook defined under path: " + path);
-            } else {
-                Hook hook = hookIt.next();
-                LOG.info("Invoke hook [{}], path: {}", hook.getName(), path);
-                hook.invoke(conf, msg);
-            }
-        } catch (Exception e) {
-            LOG.error("Exception when invoke hook", e);
-            throw DataXException.asDataXException(
-                    CommonErrorCode.HOOK_INTERNAL_ERROR, "Exception when invoke hook", e);
-        } finally {
-            Thread.currentThread().setContextClassLoader(oldClassLoader);
-        }
-    }
+//    private void doInvoke(String path) {
+//        ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
+//        try {
+//            JarLoader jarLoader = new JarLoader(new String[]{path});
+//            Thread.currentThread().setContextClassLoader(jarLoader);
+//            Iterator<Hook> hookIt = ServiceLoader.load(Hook.class).iterator();
+//            if (!hookIt.hasNext()) {
+//                LOG.warn("No hook defined under path: " + path);
+//            } else {
+//                Hook hook = hookIt.next();
+//                LOG.info("Invoke hook [{}], path: {}", hook.getName(), path);
+//                hook.invoke(conf, msg);
+//            }
+//        } catch (Exception e) {
+//            LOG.error("Exception when invoke hook", e);
+//            throw DataXException.asDataXException(
+//                    CommonErrorCode.HOOK_INTERNAL_ERROR, "Exception when invoke hook", e);
+//        } finally {
+//            Thread.currentThread().setContextClassLoader(oldClassLoader);
+//        }
+//    }
 }
