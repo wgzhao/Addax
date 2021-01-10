3df36ac67184c4bf8a011c61eb23c5029f9791a0
wgzhao <wgzhao@gmail.com>
2021-01-10 17:43:26 +0800

    Add kudu reader plugin (#97)
    Add kudu reader plugin #96

---
+++ kudureader/src/main/java/com/alibaba/datax/plugin/reader/kudureader/Key.java
@@ -0,0 +1,27 @@
+package com.alibaba.datax.plugin.reader.kudureader;
+
+/**
+ * Created by roy on 2019/12/12 1543.
+ */
+public class Key
+{
+
+    public static final String KUDU_MASTER_ADDRESSES = "masterAddress";
+
+    public static final String KUDU_TABlE_NAME = "table";
+
+    public static final String LOWER_BOUND = "lowerBound";
+
+    public static final String UPPER_BOUND = "upperBound";
+
+    public static final String SPLIT_LOWER_BOUND = "splitLowerBound";
+
+    public static final String SPLIT_UPPER_BOUND = "splitUpperBound";
+
+    public static final String SPLIT_KEY = "splitPk";
+
+    public static final String SOCKET_READ_TIMEOUT = "readTimeout";
+
+    public static final String SCAN_REQUEST_TIMEOUT = "scanTimeout";
+
+}
