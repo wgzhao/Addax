1100a1888dc27a1b1c8bf048ad2da285d85d646c
wgzhao <wgzhao@gmail.com>
2021-01-16 22:33:43 +0800

    Changes the location of plugins (#106)

---
+++ plugin/reader/dbffilereader/src/main/java/com/alibaba/datax/plugin/reader/dbffilereader/Key.java
@@ -0,0 +1,11 @@
+package com.alibaba.datax.plugin.reader.dbffilereader;
+
+/**
+ * Created by zhongtian.hu on 19-8-8.
+ */
+public class Key
+{
+    private Key() {}
+
+    public static final String PATH = "path";
+}
