8c657fd7c54db411e315779dd2d3b6078f41ffbb
wgzhao <wgzhao@gmail.com>
2021-01-17 20:21:58 +0800

    Add InfluxDB Writer plugin (#109)

---
+++ plugin/writer/influxdbwriter/src/main/java/com/alibaba/datax/plugin/writer/influxdbwriter/Key.java
@@ -0,0 +1,16 @@
+package com.alibaba.datax.plugin.writer.influxdbwriter;
+
+public class Key
+{
+    public static final String ENDPOINT = "endpoint";
+    public static final String COLUMN = "column";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String DATABASE = "database";
+    public static final String TABLE = "table";
+    public static final String QUERY_SQL = "querySql";
+    public static final String PRE_SQL = "preSql";
+    public static final String POST_SQL = "postSql";
+    public static final String CONNECTION = "connection";
+    public static final String BATCH_SIZE = "batch_size";
+}
