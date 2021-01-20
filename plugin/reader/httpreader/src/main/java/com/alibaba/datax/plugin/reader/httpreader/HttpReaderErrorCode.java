ce58778060a764f6bb05cd1d44fd5a886079b666
wgzhao <wgzhao@gmail.com>
2021-01-20 09:32:15 +0800

    Add document

--- plugin/reader/httpreader/src/main/java/com/alibaba/datax/plugin/reader/httpreader/HttpReaderErrorCode.java
+++ plugin/reader/httpreader/src/main/java/com/alibaba/datax/plugin/reader/httpreader/HttpReaderErrorCode.java
@@ -5,11 +5,11 @@ import com.alibaba.datax.common.spi.ErrorCode;
 public enum HttpReaderErrorCode
         implements ErrorCode
 {
-    REQUIRED_VALUE("HttpReader-00","您缺失了必须填写的参数值."),
+    REQUIRED_VALUE("HttpReader-00", "您缺失了必须填写的参数值."),
 
-    ILLEGAL_VALUE("HttpReader-01","您填写的参数值不合法."),
+    ILLEGAL_VALUE("HttpReader-01", "您填写的参数值不合法."),
 
-    FILE_NOT_EXISTS("HttpReader-04","您配置的没有权限读取."),
+    FILE_NOT_EXISTS("HttpReader-04", "您配置的没有权限读取."),
 
 //        OPEN_FILE_WITH_CHARSET_ERROR("HttpReader-05", "您配置的文件编码和实际文件编码不符合."),
 
@@ -18,13 +18,13 @@ public enum HttpReaderErrorCode
 //    CONFIG_INVALID_EXCEPTION("HttpReader-09", "您的参数配置错误."),
 //    RUNTIME_EXCEPTION("HttpReader-10", "出现运行时异常, 请联系我们"),
 
-    FAIL_LOGIN("HttpReader-12","登录失败,无法与http服务器建立连接."),
+    FAIL_LOGIN("HttpReader-12", "登录失败,无法与http服务器建立连接."),
 
-    FAIL_DISCONNECT("HttpReader-13","关闭http连接失败,无法与http服务器断开连接."),
+    FAIL_DISCONNECT("HttpReader-13", "关闭http连接失败,无法与http服务器断开连接."),
 
-    COMMAND_FTP_IO_EXCEPTION("HttpReader-14","与http服务器连接异常."),
+    COMMAND_FTP_IO_EXCEPTION("HttpReader-14", "与http服务器连接异常."),
     NOT_SUPPORT("HttpReader-15", "暂不支持该方式"),
-            ;
+    ;
 
     private final String code;
     private final String description;
