0cd382ec470f9dc575cc5a057198c6ec6f718936
wgzhao <wgzhao@gmail.com>
2021-03-18 21:17:52 +0800

    Use native influxDB API instead of influxdb-java (#153)
    Use native influxDB API instead of influxdb-java, fixed #146

--- plugin/reader/influxdbreader/src/main/java/com/wgzhao/datax/plugin/reader/influxdbreader/Key.java
+++ plugin/reader/influxdbreader/src/main/java/com/wgzhao/datax/plugin/reader/influxdbreader/Key.java
@@ -2,16 +2,13 @@ package com.wgzhao.datax.plugin.reader.influxdbreader;
 
 public class Key
 {
-    public static final String WHERE = "where";
     public static final String ENDPOINT = "endpoint";
     public static final String COLUMN = "column";
     public static final String USERNAME = "username";
     public static final String PASSWORD = "password";
     public static final String DATABASE = "database";
-    public static final String TABLE = "table";
     public static final String QUERY_SQL = "querySql";
     public static final String CONNECTION = "connection";
     public static final String CONNECT_TIMEOUT_SECONDS = "connTimeout";
-    public static final String READ_TIMEOUT_SECONDS = "readTimeout";
-    public static final String WRITE_TIMEOUT_SECONDS = "writeTimeout";
+    public static final String SOCKET_TIMEOUT_SECONDS = "socketTimeout";
 }
