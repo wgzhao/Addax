73e8e034601605cc413cbe333b0d29f341e75c81
wgzhao <wgzhao@gmail.com>
2021-06-25 16:14:57 +0800

    Support for read/write Parquet file with binary type

--- plugin/writer/hdfswriter/src/test/java/com/wgzhao/datax/plugin/writer/hdfswriter/TestParquetFile.java
+++ plugin/writer/hdfswriter/src/test/java/com/wgzhao/datax/plugin/writer/hdfswriter/TestParquetFile.java
@@ -24,10 +24,13 @@ import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.avro.AvroParquetWriter;
 import org.apache.parquet.column.ParquetProperties;
+import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.junit.jupiter.api.Test;
@@ -35,6 +38,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.ByteBuffer;
 import java.sql.Timestamp;
 
 public class TestParquetFile
@@ -50,6 +54,17 @@ public class TestParquetFile
         schema = parser.parse(TestParquetFile.class.getResourceAsStream("/parquet_schema.asvo"));
     }
 
+    private byte[] load_file()
+    {
+        try {
+            return IOUtils.toByteArray(TestOrcFile.class.getResourceAsStream("/datax_logo.png"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return new byte[0];
+        }
+    }
+
     @Test
     public void testParquetWrite()
     {
@@ -60,7 +75,7 @@ public class TestParquetFile
         GenericData decimalSupport = new GenericData();
         decimalSupport.addLogicalTypeConversion(new Conversions.DecimalConversion());
         Path path = new Path(file.toString());
-        try (ParquetWriter<GenericRecord> writer =  AvroParquetWriter
+        try (ParquetWriter<GenericRecord> writer = AvroParquetWriter
                 .<GenericRecord>builder(path)
                 .withRowGroupSize(ParquetWriter.DEFAULT_BLOCK_SIZE)
                 .withSchema(schema)
@@ -71,7 +86,7 @@ public class TestParquetFile
                 .withCompressionCodec(compress)
                 .withWriterVersion(ParquetProperties.WriterVersion.PARQUET_1_0)
                 .build()
-        ){
+        ) {
             GenericRecordBuilder builder = new GenericRecordBuilder(schema);
             builder.set("col1", Long.MAX_VALUE - 1);
             builder.set("col2", Integer.MAX_VALUE - 1);
@@ -80,8 +95,41 @@ public class TestParquetFile
             builder.set("col4", dec);
             java.sql.Date date = new java.sql.Date(Timestamp.valueOf("1989-06-04 13:14:15").getTime());
             builder.set("col5", date.getTime() / 1000);
-            builder.set("col6", Timestamp.valueOf("1989-06-04 13:14:15").getTime()/ 1000);
+            builder.set("col6", Timestamp.valueOf("1989-06-04 13:14:15").getTime() / 1000);
+            builder.set("col7", load_file());
             writer.write(builder.build());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testParquetRead()
+    {
+        Path parquetFilePath = new Path(filePath);
+        GenericData decimalSupport = new GenericData();
+        decimalSupport.addLogicalTypeConversion(new Conversions.DecimalConversion());
+        try (ParquetReader<GenericData.Record> reader = AvroParquetReader
+                .<GenericData.Record>builder(parquetFilePath)
+                .withDataModel(decimalSupport)
+                .withConf(new Configuration())
+                .build()) {
+            GenericData.Record gRecord = reader.read();
+            Schema schema = gRecord.getSchema();
+            for (Schema.Field field: schema.getFields()) {
+                System.out.print(field.name() + "\t");
+            }
+            System.out.println();
+            while (gRecord != null) {
+                byte[] buffer = ((ByteBuffer)gRecord.get(6)).array();
+                System.out.println(buffer.length);
+//                for ( int i=0; i<schema.getFields().size(); i++) {
+//                    System.out.print(gRecord.get(i).toString() + "\t");
+//                }
+//                System.out.println();
+                gRecord = reader.read();
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
