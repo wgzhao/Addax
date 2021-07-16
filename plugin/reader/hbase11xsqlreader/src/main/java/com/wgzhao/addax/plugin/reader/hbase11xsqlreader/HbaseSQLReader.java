22e8e0ca40a037928ed0604cde41a865c2e8c113
wgzhao <wgzhao@gmail.com>
2021-07-16 13:13:08 +0800

    Fix login failed while kerberos enabled. allow custom zookeeper server port

--- plugin/reader/hbase11xsqlreader/src/main/java/com/wgzhao/addax/plugin/reader/hbase11xsqlreader/HbaseSQLReader.java
+++ plugin/reader/hbase11xsqlreader/src/main/java/com/wgzhao/addax/plugin/reader/hbase11xsqlreader/HbaseSQLReader.java
@@ -19,13 +19,35 @@
 
 package com.wgzhao.addax.plugin.reader.hbase11xsqlreader;
 
+import com.wgzhao.addax.common.base.Key;
+import com.wgzhao.addax.common.element.BoolColumn;
+import com.wgzhao.addax.common.element.BytesColumn;
+import com.wgzhao.addax.common.element.Column;
+import com.wgzhao.addax.common.element.DateColumn;
+import com.wgzhao.addax.common.element.DoubleColumn;
+import com.wgzhao.addax.common.element.LongColumn;
 import com.wgzhao.addax.common.element.Record;
+import com.wgzhao.addax.common.element.StringColumn;
+import com.wgzhao.addax.common.exception.AddaxException;
 import com.wgzhao.addax.common.plugin.RecordSender;
+import com.wgzhao.addax.common.plugin.TaskPluginCollector;
 import com.wgzhao.addax.common.spi.Reader;
 import com.wgzhao.addax.common.util.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HbaseSQLReader
@@ -34,18 +56,29 @@ public class HbaseSQLReader
     public static class Job
             extends Reader.Job
     {
-        private HbaseSQLReaderConfig readerConfig;
+        private Configuration originalConfig;
+        private HbaseSQLHelper hbaseSQLHelper;
 
         @Override
         public void init()
         {
-            readerConfig = HbaseSQLHelper.parseConfig(this.getPluginJobConf());
+            hbaseSQLHelper = new HbaseSQLHelper(this.getPluginJobConf());
+        }
+
+        @Override
+        public void prepare()
+        {
+            originalConfig = hbaseSQLHelper.parseConfig();
         }
 
         @Override
         public List<Configuration> split(int adviceNumber)
         {
-            return HbaseSQLHelper.split(readerConfig);
+            List<Configuration> splitResultConfigs = new ArrayList<>();
+            for (int j = 0; j < adviceNumber; j++) {
+                splitResultConfigs.add(originalConfig.clone());
+            }
+            return splitResultConfigs;
         }
 
         @Override
@@ -58,61 +91,124 @@ public class HbaseSQLReader
     public static class Task
             extends Reader.Task
     {
-        private static Logger log = LoggerFactory.getLogger(Task.class);
-        private HbaseSQLReaderTask hbase11SQLReaderTask;
+        private static final Logger LOG = LoggerFactory.getLogger(Task.class);
+        private Configuration writerConfig;
 
         @Override
         public void init()
         {
-            hbase11SQLReaderTask = new HbaseSQLReaderTask(this.getPluginJobConf());
-            this.hbase11SQLReaderTask.init();
+            this.writerConfig = this.getPluginJobConf();
         }
 
         @Override
         public void prepare()
         {
-            hbase11SQLReaderTask.prepare();
+            //
         }
 
         @Override
         public void startRead(RecordSender recordSender)
         {
-            long recordNum = 0L;
-            Record record = recordSender.createRecord();
-            boolean fetchOK = true;
-            while (fetchOK) {
-                try {
-                    fetchOK = this.hbase11SQLReaderTask.readRecord(record);
-                }
-                catch (Exception e) {
-                    log.info("Read record exception", e);
-                    e.printStackTrace();
-                    super.getTaskPluginCollector().collectDirtyRecord(record, e);
-                    record = recordSender.createRecord();
-                    continue;
-                }
-                if (fetchOK) {
+            LOG.info("Begin reading.....");
+            PreparedStatement preparedStatement;
+            ResultSet resultSet;
+            Record record;
+            String querySql = this.writerConfig.getString(Key.QUERY_SQL);
+            String jdbcUrl = this.writerConfig.getString(Key.JDBC_URL);
+            LOG.info("Query with [{}]", querySql);
+            try {
+                Connection connection = DriverManager.getConnection(jdbcUrl);
+                preparedStatement = connection.prepareStatement(querySql);
+                resultSet = preparedStatement.executeQuery();
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                while (resultSet.next()) {
+                    record = transportOneRecord(recordSender, resultSet, metaData, metaData.getColumnCount(), getTaskPluginCollector());
                     recordSender.sendToWriter(record);
-                    recordNum++;
-                    if (recordNum % 10000 == 0) {
-                        log.info("already read record num is {}", recordNum);
-                    }
-                    record = recordSender.createRecord();
                 }
+                recordSender.flush();
             }
-            recordSender.flush();
+            catch (SQLException e) {
+                throw AddaxException.asAddaxException(
+                        HbaseSQLReaderErrorCode.HBASE_CONNECTION_ERROR,
+                        e.getMessage()
+                );
+            }
+            LOG.info("End reading.....");
+        }
+
+        private Record transportOneRecord(RecordSender recordSender, ResultSet resultSet, ResultSetMetaData rmd, int columnNum, TaskPluginCollector taskPluginCollector)
+        {
+            Record record = recordSender.createRecord();
+            Column column;
+            try {
+                for (int i = 1; i <= columnNum; i++) {
+                    Object value = resultSet.getObject(i);
+                    switch (rmd.getColumnType(i)) {
+                        case Types.CHAR:
+                        case Types.VARCHAR:
+                            column = new StringColumn((String) value);
+                            break;
+                        case Types.BINARY:
+                        case Types.VARBINARY:
+                            column = new BytesColumn((byte[]) value);
+                            break;
+                        case Types.BOOLEAN:
+                            column = new BoolColumn((Boolean) value);
+                            break;
+                        case Types.INTEGER:
+                            column = new LongColumn((Integer) value);
+                            break;
+                        case Types.TINYINT:
+                            column = new LongColumn(((Byte) value).longValue());
+                            break;
+                        case Types.SMALLINT:
+                            column = new LongColumn(((Short) value).longValue());
+                            break;
+                        case Types.BIGINT:
+                            column = new LongColumn((Long) value);
+                            break;
+                        case Types.FLOAT:
+                            column = new DoubleColumn(((Float) value).doubleValue());
+                            break;
+                        case Types.DECIMAL:
+                            column = new DoubleColumn(((BigDecimal) value));
+                            break;
+                        case Types.DOUBLE:
+                            column = new DoubleColumn((Double) value);
+                            break;
+                        case Types.DATE:
+                            column = new DateColumn((Date) value);
+                            break;
+                        case Types.TIME:
+                            column = new DateColumn((Time) value);
+                            break;
+                        case Types.TIMESTAMP:
+                            column = new DateColumn((Timestamp) value);
+                            break;
+                        default:
+                            throw AddaxException.asAddaxException(
+                                    HbaseSQLReaderErrorCode.PHOENIX_COLUMN_TYPE_CONVERT_ERROR, "The data type " + rmd.getColumnType(i) + " is unsupported yet");
+                    }
+                    record.addColumn(column);
+                }
+                return record;
+            }
+            catch (SQLException e) {
+                taskPluginCollector.collectDirtyRecord(record, e);
+                return null;
+            }
         }
 
         @Override
         public void post()
         {
-            super.post();
+            //
         }
 
         @Override
         public void destroy()
         {
-            this.hbase11SQLReaderTask.destroy();
+            //
         }
     }
 }
