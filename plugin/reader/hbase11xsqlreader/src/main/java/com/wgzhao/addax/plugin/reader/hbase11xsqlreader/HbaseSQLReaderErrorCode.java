0d82582b079d8491ce82a2c41f4d8d3e8e8a91fc
wgzhao <wgzhao@gmail.com>
2021-07-16 13:32:19 +0800

    Fix login failed while kerberos enabled. allow custom zookeeper server port (#241)
    1. Fix the problem that connecting hbase fails when kerberos is enabled
    2. Allow use different zookeeper port, previously the port configuration was hard-coded

--- plugin/reader/hbase11xsqlreader/src/main/java/com/wgzhao/addax/plugin/reader/hbase11xsqlreader/HbaseSQLReaderErrorCode.java
+++ plugin/reader/hbase11xsqlreader/src/main/java/com/wgzhao/addax/plugin/reader/hbase11xsqlreader/HbaseSQLReaderErrorCode.java
@@ -24,16 +24,18 @@ import com.wgzhao.addax.common.spi.ErrorCode;
 public enum HbaseSQLReaderErrorCode
         implements ErrorCode
 {
-    REQUIRED_VALUE("Hbasewriter-00", "您缺失了必须填写的参数值."),
-    ILLEGAL_VALUE("Hbasewriter-01", "您填写的参数值不合法."),
-    GET_PHOENIX_COLUMN_ERROR("Hbasewriter-02", "获取phoenix表的列值错误"),
-    GET_PHOENIX_CONNECTIONINFO_ERROR("Hbasewriter-03", "获取phoenix服务的zkurl错误"),
-    GET_PHOENIX_SPLITS_ERROR("Hbasewriter-04", "获取phoenix的split信息错误"),
-    PHOENIX_CREATEREADER_ERROR("Hbasewriter-05", "获取phoenix的reader错误"),
-    PHOENIX_READERINIT_ERROR("Hbasewriter-06", "phoenix reader的初始化错误"),
-    PHOENIX_COLUMN_TYPE_CONVERT_ERROR("Hbasewriter-07", "phoenix的列类型转换错误"),
-//    PHOENIX_RECORD_READ_ERROR("Hbasewriter-08", "phoenix record 读取错误"),
-    PHOENIX_READER_CLOSE_ERROR("Hbasewriter-09", "phoenix reader 的close错误");
+    REQUIRED_VALUE("HBaseReader-00", "您缺失了必须填写的参数值."),
+    ILLEGAL_VALUE("HBaseReader-01", "您填写的参数值不合法."),
+    GET_PHOENIX_COLUMN_ERROR("HBaseReader-02", "获取phoenix表的列值错误"),
+    GET_PHOENIX_CONNECTIONINFO_ERROR("HBaseReader-03", "获取phoenix服务的zkurl错误"),
+    GET_PHOENIX_SPLITS_ERROR("HBaseReader-04", "获取phoenix的split信息错误"),
+    PHOENIX_CREATEREADER_ERROR("HBaseReader-05", "获取phoenix的reader错误"),
+    PHOENIX_READERINIT_ERROR("HBaseReader-06", "phoenix reader的初始化错误"),
+    PHOENIX_COLUMN_TYPE_CONVERT_ERROR("HBaseReader-07", "phoenix的列类型转换错误"),
+    //    PHOENIX_RECORD_READ_ERROR("HBaseReader-08", "phoenix record 读取错误"),
+    PHOENIX_READER_CLOSE_ERROR("HBaseReader-09", "phoenix reader 的close错误"),
+    KERBEROS_LOGIN_ERROR("HBaseReader-10", "KERBEROS认证失败"),
+    HBASE_CONNECTION_ERROR("HBaseReader-11", "连接 HBase 失败");
 
     private final String code;
     private final String description;
