f3c768c5282eee71a55322bef0fe49f5837fad0c
wgzhao <wgzhao@gmail.com>
2021-08-19 16:40:59 +0800

    [kudureader] Add support for configuring `where` to set more filters (#298)
    [kudureader] Add support for configuring where to set more filters, Fixed #252, #292

--- plugin/reader/kudureader/src/main/java/com/wgzhao/addax/plugin/reader/kudureader/KuduReader.java
+++ plugin/reader/kudureader/src/main/java/com/wgzhao/addax/plugin/reader/kudureader/KuduReader.java
@@ -19,6 +19,7 @@
 
 package com.wgzhao.addax.plugin.reader.kudureader;
 
+import com.google.common.collect.ImmutableMap;
 import com.wgzhao.addax.common.element.BoolColumn;
 import com.wgzhao.addax.common.element.BytesColumn;
 import com.wgzhao.addax.common.element.DateColumn;
@@ -41,11 +42,18 @@ import org.apache.kudu.client.KuduTable;
 import org.apache.kudu.client.RowResult;
 import org.apache.kudu.client.RowResultIterator;
 
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.wgzhao.addax.common.base.Key.COLUMN;
+import static com.wgzhao.addax.common.base.Key.WHERE;
 
 /**
  * Kudu reader plugin
@@ -57,7 +65,6 @@ public class KuduReader
     public static class Job
             extends Reader.Job
     {
-
         private Configuration originalConfig = null;
 
         private String splitKey;
@@ -65,6 +72,16 @@ public class KuduReader
         private String lowerBound;
 
         private String upperBound;
+        // match where clause such as age > 18
+        private static final String PATTERN_FOR_WHERE = "^(\\w+)\\s+(=|>|>=|<|<=)\\s+(.*)$";
+        private static final Pattern pattern = Pattern.compile(PATTERN_FOR_WHERE);
+        private static final Map<String, KuduPredicate.ComparisonOp> KUDU_OPERATORS = ImmutableMap.of(
+                "=", KuduPredicate.ComparisonOp.EQUAL,
+                ">", KuduPredicate.ComparisonOp.GREATER,
+                ">=", KuduPredicate.ComparisonOp.GREATER_EQUAL,
+                "<", KuduPredicate.ComparisonOp.LESS,
+                "<=", KuduPredicate.ComparisonOp.LESS_EQUAL
+        );
 
         @Override
         public List<Configuration> split(int adviceNumber)
@@ -105,6 +122,42 @@ public class KuduReader
         }
 
         @Override
+        public void prepare()
+        {
+            List<String> where = this.originalConfig.getList(WHERE, String.class);
+            if (where != null && !where.isEmpty()) {
+                List<Configuration> result = new ArrayList<>();
+                Matcher matcher;
+
+                for (String w : where) {
+                    matcher = pattern.matcher(w);
+                    while (matcher.find()) {
+                        if (matcher.groupCount() == 3) {
+                            if (KUDU_OPERATORS.containsKey(matcher.group(2))) {
+                                Configuration conf = Configuration.from(
+                                        String.format("{\"field\": \"%s\", \"op\": \"%s\", \"value\": \"%s\"}",
+                                                matcher.group(1).trim(), matcher.group(2).trim(), matcher.group(3).trim()));
+                                result.add(conf);
+                            }
+                            else {
+                                throw AddaxException.asAddaxException(KuduReaderErrorCode.ILLEGAL_VALUE,
+                                        "operator '" + matcher.group(2) + "' is unsupported");
+                            }
+                        }
+                        else {
+                            throw AddaxException.asAddaxException(KuduReaderErrorCode.ILLEGAL_VALUE,
+                                    "Illegal where clause: " + w);
+                        }
+                    }
+                }
+
+                if (!result.isEmpty()) {
+                    this.originalConfig.set(WHERE, result);
+                }
+            }
+        }
+
+        @Override
         public void init()
         {
             originalConfig = super.getPluginJobConf();
@@ -137,6 +190,16 @@ public class KuduReader
         private Long scanRequestTimeout;
         private List<String> columns;
         private boolean specifyColumn = false;
+        List<KuduPredicate> customPredicate;
+        private static final Map<String, KuduPredicate.ComparisonOp> KUDU_OPERATORS = ImmutableMap.of(
+                "=", KuduPredicate.ComparisonOp.EQUAL,
+                ">", KuduPredicate.ComparisonOp.GREATER,
+                ">=", KuduPredicate.ComparisonOp.GREATER_EQUAL,
+                "<", KuduPredicate.ComparisonOp.LESS,
+                "<=", KuduPredicate.ComparisonOp.LESS_EQUAL
+        );
+
+        List<Configuration> where;
 
         @Override
         public void startRead(RecordSender recordSender)
@@ -177,7 +240,7 @@ public class KuduReader
                 if (specifyColumn) {
                     // judge specific column exists or not
 
-                    for (String column: columns) {
+                    for (String column : columns) {
                         if (!schema.hasColumn(column)) {
                             throw AddaxException.asAddaxException(
                                     KuduReaderErrorCode.ILLEGAL_VALUE,
@@ -189,9 +252,15 @@ public class KuduReader
                 }
             }
 
+            if (!where.isEmpty()) {
+                List<KuduPredicate> customPredicate = processWhere(where, schema);
+                for (KuduPredicate p : customPredicate) {
+                    kuduScannerBuilder.addPredicate(p);
+                }
+            }
+
             kuduScanner = kuduScannerBuilder.build();
 
-//            List<ColumnSchema> columnSchemas = schema.getColumns();
             List<ColumnSchema> columnSchemas = kuduScanner.getProjectionSchema().getColumns();
 
             while (kuduScanner.hasMoreRows()) {
@@ -265,7 +334,6 @@ public class KuduReader
                             break;
                         }
                     }
-
                     if (!isDirtyRecord) {
                         recordSender.sendToWriter(record);
                     }
@@ -292,16 +360,112 @@ public class KuduReader
             long socketReadTimeoutMs = readerSliceConfig.getLong(KuduKey.SOCKET_READ_TIMEOUT, 10) * 1000L;
             scanRequestTimeout = readerSliceConfig.getLong(KuduKey.SCAN_REQUEST_TIMEOUT, 20L) * 1000L;
             KuduClient.KuduClientBuilder kuduClientBuilder = (new KuduClient.KuduClientBuilder(masterAddresses));
-            kuduClientBuilder.defaultSocketReadTimeoutMs(socketReadTimeoutMs);
+            kuduClientBuilder.defaultOperationTimeoutMs(socketReadTimeoutMs);
 
             kuduClient = kuduClientBuilder.build();
             lowerBound = readerSliceConfig.getString(KuduKey.SPLIT_LOWER_BOUND);
             upperBound = readerSliceConfig.getString(KuduKey.SPLIT_UPPER_BOUND);
             splitKey = readerSliceConfig.getString(KuduKey.SPLIT_KEY);
             columns = readerSliceConfig.getList(COLUMN, String.class);
-            if (! columns.isEmpty()) {
+            if (!columns.isEmpty()) {
                 specifyColumn = columns.size() != 1 || (!"*".equals(columns.get(0)) && !"\"*\"".equals(columns.get(0)));
             }
+            where = readerSliceConfig.getListConfiguration(WHERE);
+//            processWhere(where);
+        }
+
+        private void processWhere(List<Configuration> where)
+        {
+            String field;
+            KuduPredicate.ComparisonOp op;
+            KuduPredicate predicate;
+            for (Configuration conf : where) {
+                field = conf.getString("field");
+                op = KUDU_OPERATORS.get(conf.getString("op"));
+                ColumnSchema column;
+                String value = conf.getString("value");
+                if (value.startsWith("'")) {
+                    column = new ColumnSchema.ColumnSchemaBuilder(field, Type.VARCHAR).build();
+                    predicate = KuduPredicate.newComparisonPredicate(column, op, value);
+                }
+                else if (value.indexOf('.') > 0) {
+                    column = new ColumnSchema.ColumnSchemaBuilder(field, Type.DOUBLE).build();
+                    predicate = KuduPredicate.newComparisonPredicate(column, op, Double.valueOf(value));
+                }
+                else {
+                    column = new ColumnSchema.ColumnSchemaBuilder(field, Type.INT32).build();
+                    predicate = KuduPredicate.newComparisonPredicate(column, op, Long.parseLong(value));
+                }
+                this.customPredicate.add(predicate);
+            }
+        }
+
+        /**
+         * convert sql-format where to kudu {@link KuduPredicate} format
+         * "age &gt; 1" as assumed where clause , it will be convert into
+         * <pre>
+         *      KuduPredicate.newComparisonPredicate("age", KuduPredicate.ComparisonOp.GREATER, 1);
+         * </pre>
+         *
+         * @param where List of configuration, each element like <code>{"field":"age", "op": "&gt;", "value": 1}</code>
+         * @return list of {@link KuduPredicate}
+         */
+        private List<KuduPredicate> processWhere(List<Configuration> where, Schema schema)
+        {
+            List<KuduPredicate> customPredicate = new ArrayList<>();
+            String field;
+            KuduPredicate.ComparisonOp op;
+            KuduPredicate predicate;
+            for (Configuration conf : where) {
+                field = conf.getString("field");
+                op = KUDU_OPERATORS.get(conf.getString("op"));
+                if (!schema.hasColumn(field)) {
+                    throw AddaxException.asAddaxException(
+                            KuduReaderErrorCode.ILLEGAL_VALUE,
+                            "column '" + field + "' in where clause does not exists in the table '" + tableName + "'"
+                    );
+                }
+                ColumnSchema column = schema.getColumn(field);
+                String value = conf.getString("value");
+
+                switch (column.getType()) {
+                    case INT8:
+                    case INT16:
+                    case INT32:
+                    case INT64:
+                        predicate = KuduPredicate.newComparisonPredicate(column, op, Long.parseLong(value));
+                        break;
+                    case BOOL:
+                        predicate = KuduPredicate.newComparisonPredicate(column, op, Boolean.valueOf(value));
+                        break;
+                    case STRING:
+                    case VARCHAR:
+                        predicate = KuduPredicate.newComparisonPredicate(column, op, value);
+                        break;
+                    case DATE:
+                        predicate = KuduPredicate.newComparisonPredicate(column, op, Date.valueOf(value));
+                        break;
+                    case FLOAT:
+                        predicate = KuduPredicate.newComparisonPredicate(column, op, Float.valueOf(value));
+                        break;
+                    case DOUBLE:
+                        predicate = KuduPredicate.newComparisonPredicate(column, op, Double.valueOf(value));
+                        break;
+                    case DECIMAL:
+                        predicate = KuduPredicate.newComparisonPredicate(column, op, new BigDecimal(value));
+                        break;
+                    case BINARY:
+                        predicate = KuduPredicate.newComparisonPredicate(column, op, value.getBytes(StandardCharsets.UTF_8));
+                        break;
+                    case UNIXTIME_MICROS:
+                        predicate = KuduPredicate.newComparisonPredicate(column, op, Timestamp.valueOf(value));
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected type: " + column.getType());
+                }
+                customPredicate.add(predicate);
+            }
+            return customPredicate;
         }
 
         @Override
