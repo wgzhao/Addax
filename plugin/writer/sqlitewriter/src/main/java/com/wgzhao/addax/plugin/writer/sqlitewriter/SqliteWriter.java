22331f3e8697a9cf2ad2ed40135f8b5b336f6eb5
wgzhao <wgzhao@gmail.com>
2021-08-23 09:44:27 +0800

    [sqlitewriter] Introduce a new sqlitewriter plugin (#306)

---
+++ plugin/writer/sqlitewriter/src/main/java/com/wgzhao/addax/plugin/writer/sqlitewriter/SqliteWriter.java
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.wgzhao.addax.plugin.writer.sqlitewriter;
+
+import com.wgzhao.addax.common.base.Key;
+import com.wgzhao.addax.common.plugin.RecordReceiver;
+import com.wgzhao.addax.common.spi.Writer;
+import com.wgzhao.addax.common.util.Configuration;
+import com.wgzhao.addax.rdbms.util.DataBaseType;
+import com.wgzhao.addax.rdbms.writer.CommonRdbmsWriter;
+
+import java.util.List;
+
+import static com.wgzhao.addax.common.base.Key.PASSWORD;
+import static com.wgzhao.addax.common.base.Key.USERNAME;
+
+public class SqliteWriter
+        extends Writer
+{
+    private static final DataBaseType DATABASE_TYPE = DataBaseType.SQLite;
+
+    public static class Job
+            extends Writer.Job
+    {
+        private Configuration originalConfig = null;
+        private CommonRdbmsWriter.Job commonRdbmsWriterJob;
+
+        @Override
+        public void preCheck()
+        {
+            this.init();
+            this.commonRdbmsWriterJob.writerPreCheck(this.originalConfig, DATABASE_TYPE);
+        }
+
+        @Override
+        public void init()
+        {
+            this.originalConfig = super.getPluginJobConf();
+            // SQLite does not minder username and password
+            this.originalConfig.set(USERNAME, "addax");
+            this.originalConfig.set(PASSWORD, "");
+            this.commonRdbmsWriterJob = new CommonRdbmsWriter.Job(DATABASE_TYPE);
+            this.commonRdbmsWriterJob.init(this.originalConfig);
+        }
+
+        @Override
+        public void prepare()
+        {
+            this.commonRdbmsWriterJob.prepare(this.originalConfig);
+        }
+
+        @Override
+        public List<Configuration> split(int mandatoryNumber)
+        {
+            return this.commonRdbmsWriterJob.split(this.originalConfig, mandatoryNumber);
+        }
+
+        // 一般来说，是需要推迟到 task 中进行post 的执行（单表情况例外）
+        @Override
+        public void post()
+        {
+            this.commonRdbmsWriterJob.post(this.originalConfig);
+        }
+
+        @Override
+        public void destroy()
+        {
+            this.commonRdbmsWriterJob.destroy(this.originalConfig);
+        }
+    }
+
+    public static class Task
+            extends Writer.Task
+    {
+        private Configuration writerSliceConfig;
+        private CommonRdbmsWriter.Task commonRdbmsWriterTask;
+
+        @Override
+        public void init()
+        {
+            this.writerSliceConfig = super.getPluginJobConf();
+            this.commonRdbmsWriterTask = new CommonRdbmsWriter.Task(DATABASE_TYPE);
+            this.commonRdbmsWriterTask.init(this.writerSliceConfig);
+        }
+
+        @Override
+        public void prepare()
+        {
+            this.commonRdbmsWriterTask.prepare(this.writerSliceConfig);
+        }
+
+        //TODO 改用连接池，确保每次获取的连接都是可用的（注意：连接可能需要每次都初始化其 session）
+        public void startWrite(RecordReceiver recordReceiver)
+        {
+            this.commonRdbmsWriterTask.startWrite(recordReceiver, this.writerSliceConfig,
+                    super.getTaskPluginCollector());
+        }
+
+        @Override
+        public void post()
+        {
+            this.commonRdbmsWriterTask.post(this.writerSliceConfig);
+        }
+
+        @Override
+        public void destroy()
+        {
+            this.commonRdbmsWriterTask.destroy(this.writerSliceConfig);
+        }
+
+        @Override
+        public boolean supportFailOver()
+        {
+            String writeMode = writerSliceConfig.getString(Key.WRITE_MODE);
+            return "replace".equalsIgnoreCase(writeMode);
+        }
+    }
+}
