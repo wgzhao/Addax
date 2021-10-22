d1c665442b94d31c281bf7fe3b659c7cc80a3f4b
wgzhao <wgzhao@gmail.com>
2021-10-22 21:56:26 +0800

    Deployed 576ba307 to 4.0.4 with MkDocs 1.2.3 and mike 1.1.2

---
+++ 4.0.4/assets/sql/clickhouse.sql
@@ -0,0 +1,41 @@
+CREATE TABLE ck_addax (
+    c_int8 Int8,
+    c_int16 Int16,
+    c_int32 Int32,
+    c_int64 Int64,
+    c_uint8 UInt8,
+    c_uint16 UInt16,
+    c_uint32 UInt32,
+    c_uint64 UInt64,
+    c_float32 Float32,
+    c_float64 Float64,
+    c_decimal Decimal(38,10),
+    c_string String,
+    c_fixstr FixedString(36),
+    c_uuid UUID,
+    c_date Date,
+    c_datetime DateTime('Asia/Chongqing'),
+    c_datetime64 DateTime64(3, 'Asia/Chongqing'),
+    c_enum Enum('hello' = 1, 'world'=2)
+) ENGINE = MergeTree() ORDER BY (c_int8, c_int16) SETTINGS index_granularity = 8192;
+
+insert into ck_addax values(
+    127,
+    -32768,
+    2147483647,
+    -9223372036854775808,
+    255,
+    65535,
+    4294967295,
+    18446744073709551615,
+    0.999999999999,
+    0.99999999999999999,
+    1234567891234567891234567891.1234567891,
+    'Hello String',
+    '2c:16:db:a3:3a:4f',
+    '5F042A36-5B0C-4F71-ADFD-4DF4FCA1B863',
+    '2021-01-01',
+    '2021-01-01 00:00:00',
+    '2021-01-01 00:00:00',
+    'hello'
+);
