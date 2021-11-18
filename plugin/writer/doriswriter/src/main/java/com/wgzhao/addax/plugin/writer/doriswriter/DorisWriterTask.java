db083421ae441902143f705969594b7a13949efb
wgzhao <wgzhao@gmail.com>
2021-11-18 17:02:21 +0800

    [doriswriter] merge office doriswriter codes (#448)

--- plugin/writer/doriswriter/src/main/java/com/wgzhao/addax/plugin/writer/doriswriter/DorisWriterTask.java
+++ plugin/writer/doriswriter/src/main/java/com/wgzhao/addax/plugin/writer/doriswriter/DorisWriterTask.java
@@ -25,6 +25,7 @@ import com.wgzhao.addax.common.exception.AddaxException;
 import com.wgzhao.addax.common.plugin.RecordReceiver;
 import com.wgzhao.addax.common.plugin.TaskPluginCollector;
 import com.wgzhao.addax.common.util.Configuration;
+import com.wgzhao.addax.rdbms.util.DBUtilErrorCode;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -43,17 +44,20 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.UUID;
 
+import static com.wgzhao.addax.common.base.Constant.DEFAULT_BATCH_BYTE_SIZE;
+
 public class DorisWriterTask
 {
     private static final Logger LOG = LoggerFactory.getLogger(DorisWriterTask.class);
+    private static final String DEFAULT_LABEL_PREFIX = "addax_doris_writer_";
     private final Configuration configuration;
-    private String username;
-    private String password;
-    private String loadUrl;
     private List<String> column;
     private static final String SEPARATOR = "|";
-    private int batchSize;
-    private HttpClientBuilder httpClientBuilder;
+    private long batchSize;
+    private long batchByteSize;
+    private DorisCodec rowCodec;
+    private int batchNum = 0;
+    private DorisWriterEmitter dorisWriterEmitter;
 
     public DorisWriterTask(Configuration configuration) {this.configuration = configuration;}
 
@@ -61,114 +65,76 @@ public class DorisWriterTask
     {
         List<Object> connList = configuration.getList(Key.CONNECTION);
         Configuration conn = Configuration.from(connList.get(0).toString());
-        String endpoint = conn.getString(Key.ENDPOINT);
-        String table = conn.getString(Key.TABLE);
-        String database = conn.getString(Key.DATABASE);
+        if ("csv".equalsIgnoreCase(configuration.getString(DorisKey.FORMAT, "csv"))) {
+            this.rowCodec = new DorisCsvCodec(configuration.getList(DorisKey.COLUMN, String.class), SEPARATOR);
+        }
+        else {
+            this.rowCodec = new DorisJsonCodec(configuration.getList(DorisKey.COLUMN, String.class));
+        }
+        this.dorisWriterEmitter = new DorisWriterEmitter(configuration);
+
         this.column = configuration.getList(Key.COLUMN, String.class);
         // 如果 column 填写的是 * ，直接设置为null，方便后续判断
-        if (this.column!= null && this.column.size() == 1 && "*".equals(this.column.get(0))) {
+        if (this.column != null && this.column.size() == 1 && "*".equals(this.column.get(0))) {
             this.column = null;
         }
         this.batchSize = configuration.getInt(Key.BATCH_SIZE, 1024);
-        this.username = configuration.getString(Key.USERNAME);
-        this.password = configuration.getString(Key.PASSWORD, null);
-        this.loadUrl = String.format("%s/api/%s/%s/_stream_load", endpoint, database, table);
-        this.httpClientBuilder = HttpClients
-                .custom()
-                .setRedirectStrategy(new DefaultRedirectStrategy()
-                {
-                    @Override
-                    protected boolean isRedirectable(String method)
-                    {
-                        return true;
-                    }
-                });
-        LOG.info("connect DorisDB with {}", this.loadUrl);
-    }
-
-    private String basicAuthHeader(String username, String password)
-    {
-        String tokenEncode = username + ":" + password;
-        byte[] encoded = Base64.encodeBase64(tokenEncode.getBytes(StandardCharsets.UTF_8));
-        return "Basic " + new String(encoded);
+        this.batchByteSize = configuration.getLong(DorisKey.BATCH_BYTE_SIZE, DEFAULT_BATCH_BYTE_SIZE);
     }
 
     public void startWrite(RecordReceiver recordReceiver, TaskPluginCollector taskPluginCollector)
     {
-        Record record;
-        int currSize = 0;
-        StringBuilder stringBuilder = new StringBuilder();
-        while ((record = recordReceiver.getFromReader()) != null) {
-            int len = record.getColumnNumber();
-            if (this.column != null && len != this.column.size()) {
-                throw AddaxException.asAddaxException(
-                        DorisWriterErrorCode.ILLEGAL_VALUE,
-                        String.format("源字段数和目标字段数不匹配，源字段数为%s, 目标字段数为%s", len, this.column.size())
-                );
-            }
-            StringBuilder oneRow = new StringBuilder();
-            for (int i = 0; i < len; i++) {
-                if (record.getColumn(i).getRawData() != null) {
-                    oneRow.append(record.getColumn(i).asString());
+        String lineDelimiter = configuration.getString(DorisKey.LINE_DELIMITER, "\n");
+
+        try {
+            DorisFlushBatch flushBatch = new DorisFlushBatch(lineDelimiter);
+            Record record;
+            long batchCount = 0L;
+            long batchSize = 0L;
+            while ((record = recordReceiver.getFromReader()) != null) {
+                int len = record.getColumnNumber();
+                // check column size
+                if (this.column != null && len != this.column.size()) {
+                    throw AddaxException.asAddaxException(
+                            DorisWriterErrorCode.ILLEGAL_VALUE,
+                            String.format("config writer column info error. because the column number of reader is :%s" +
+                                    "and the column number of writer is:%s. please check your job config json", len, this.column.size())
+                    );
                 }
-                if (i < len - 1) {
-                    oneRow.append(SEPARATOR);
+                // codec record
+                final String recordStr = this.rowCodec.serialize(record);
+                // put into buffer
+                flushBatch.putData(recordStr);
+                batchCount++;
+                batchSize += recordStr.length();
+                // trigger buffer
+                if (batchCount >= this.batchSize || batchSize >= this.batchByteSize) {
+                    flush(flushBatch);
+                    // clear buffer
+                    batchCount = 0L;
+                    batchByteSize = 0L;
+                    flushBatch = new DorisFlushBatch(lineDelimiter);
                 }
-            }
-            oneRow.append("\n");
-            stringBuilder.append(oneRow);
-            currSize++;
-            if (currSize >= this.batchSize) {
-                stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-                sendData(stringBuilder.toString());
-                currSize = 0;
-                stringBuilder.setLength(0);
+            } // end of while
+            // flush the last batch
+            if (flushBatch.getSize() > 0) {
+                flush(flushBatch);
             }
         }
-        if (stringBuilder.length() > 0) {
-            sendData(stringBuilder.toString());
+        catch (Exception e) {
+            throw AddaxException.asAddaxException(DBUtilErrorCode.WRITE_DATA_ERROR, e);
         }
     }
 
-    private void sendData(String content)
+    private void flush(DorisFlushBatch flushBatch)
+            throws IOException
     {
-        try (CloseableHttpClient client = this.httpClientBuilder.build()) {
-            HttpPut put = new HttpPut(this.loadUrl);
-            StringEntity entity = new StringEntity(content, "UTF-8");
-            put.setHeader(HttpHeaders.EXPECT, "100-continue");
-            if (this.username != null && this.password != null) {
-                put.setHeader(HttpHeaders.AUTHORIZATION, basicAuthHeader(this.username, this.password));
-            }
-            put.setHeader("column_separator", SEPARATOR);
-            // the label header is optional, not necessary
-            // use label header can ensure at most once semantics
-            put.setHeader("label", UUID.randomUUID().toString());
-            if (this.column != null ) {
-                put.setHeader("columns", String.join(",", this.column));
-            }
-            put.setEntity(entity);
+        final String label = getStreamLoadLabel();
+        flushBatch.setLabel(label);
+        dorisWriterEmitter.doStreamLoad(flushBatch);
+    }
 
-            try (CloseableHttpResponse response = client.execute(put)) {
-                String loadResult = "";
-                if (response.getEntity() != null) {
-                    loadResult = EntityUtils.toString(response.getEntity());
-                }
-                int statusCode = response.getStatusLine().getStatusCode();
-                // statusCode 200 just indicates that doris be service is ok, not stream load
-                // you should see the output content to find whether stream load is success
-                if (statusCode != 200) {
-                    throw AddaxException.asAddaxException(
-                            DorisWriterErrorCode.WRITER_ERROR,
-                            String.format("Stream load failed, statusCode=%s load result=%s", statusCode, loadResult)
-                    );
-                }
-            }
-        }
-        catch (IOException e) {
-            throw AddaxException.asAddaxException(
-                    DorisWriterErrorCode.CONNECT_ERROR,
-                    String.format("Failed to connect Doris server with: %s, %s", this.loadUrl, e)
-            );
-        }
+    private String getStreamLoadLabel() {
+        return DEFAULT_LABEL_PREFIX + UUID.randomUUID() + "_" + (batchNum++);
     }
 }
