bcbd5b1f2d943a787bae93484da2e8af00a87569
wgzhao <wgzhao@gmail.com>
2021-11-25 16:07:52 +0800

    [jsonfilereader] tiny code cleanup

--- plugin/reader/jsonfilereader/src/main/java/com/wgzhao/addax/plugin/reader/jsonfilereader/JsonReaderErrorCode.java
+++ plugin/reader/jsonfilereader/src/main/java/com/wgzhao/addax/plugin/reader/jsonfilereader/JsonReaderErrorCode.java
@@ -24,18 +24,17 @@ import com.wgzhao.addax.common.spi.ErrorCode;
 public enum JsonReaderErrorCode
         implements ErrorCode
 {
-    REQUIRED_VALUE("JsonFilereader-00", "您缺失了必须填写的参数值."),
-    ILLEGAL_VALUE("JsonFilereader-01", "您填写的参数值不合法."),
-    MIXED_INDEX_VALUE("JsonFilereader-02", "您的列信息配置同时包含了index,value."),
-    NO_INDEX_VALUE("JsonFilereader-03", "您明确的配置列信息,但未填写相应的index,value."),
-    FILE_NOT_EXISTS("JsonFilereader-04", "您配置的目录文件路径不存在."),
-//    OPEN_FILE_WITH_CHARSET_ERROR("JsonFilereader-05", "您配置的文件编码和实际文件编码不符合."),
-    OPEN_FILE_ERROR("JsonFilereader-06", "您配置的文件在打开时异常,建议您检查源目录是否有隐藏文件,管道文件等特殊文件."),
-    READ_FILE_IO_ERROR("JsonFilereader-07", "您配置的文件在读取时出现IO异常."),
-    SECURITY_NOT_ENOUGH("JsonFilereader-08", "您缺少权限执行相应的文件操作."),
-    CONFIG_INVALID_EXCEPTION("JsonFilereader-09", "您的参数配置错误."),
-    NOT_SUPPORT_TYPE("JsonFilereader-10", "您配置的列类型暂不支持."),
-    EMPTY_DIR_EXCEPTION("JsonFilereader-11", "您尝试读取的文件目录为空."),
+    REQUIRED_VALUE("JsonFilereader-00", "Missing required value"),
+    ILLEGAL_VALUE("JsonFilereader-01", "Illegal value"),
+    MIXED_INDEX_VALUE("JsonFilereader-02", "Both configure index and value."),
+    NO_INDEX_VALUE("JsonFilereader-03", "You specify columns, but not configure index and value "),
+    FILE_NOT_EXISTS("JsonFilereader-04", "Directory not exists."),
+    OPEN_FILE_ERROR("JsonFilereader-06", "Failed to open the file."),
+    READ_FILE_IO_ERROR("JsonFilereader-07", "IOException occurred when open file"),
+    SECURITY_NOT_ENOUGH("JsonFilereader-08", "Permission denied"),
+    CONFIG_INVALID_EXCEPTION("JsonFilereader-09", "incorrect configure."),
+    NOT_SUPPORT_TYPE("JsonFilereader-10", "The type is unsupported."),
+    EMPTY_DIR_EXCEPTION("JsonFilereader-11", "Empty directory"),
     ;
 
     private final String code;
@@ -62,8 +61,7 @@ public enum JsonReaderErrorCode
     @Override
     public String toString()
     {
-        return String.format("Code:[%s], Description:[%s].", this.code,
-                this.description);
+        return String.format("Code:[%s], Description:[%s].", this.code, this.description);
     }
 }
 
