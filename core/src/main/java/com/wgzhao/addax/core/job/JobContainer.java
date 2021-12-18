b5fd779ea778cd733e179cd06aa5f98a5058ca83
wgzhao <wgzhao@gmail.com>
2021-12-18 20:23:07 +0800

    [core] revert to prev commit to fix null point exception
    Fix #475 #476

--- core/src/main/java/com/wgzhao/addax/core/job/JobContainer.java
+++ core/src/main/java/com/wgzhao/addax/core/job/JobContainer.java
@@ -129,8 +129,6 @@ public class JobContainer
 
                 LOG.debug("jobContainer starts to do init ...");
                 this.init();
-                LOG.debug("jobContainer starts to do preCheck ...");
-                this.preCheck();
                 LOG.debug("jobContainer starts to do prepare ...");
                 this.prepare();
                 LOG.debug("jobContainer starts to do split ...");
