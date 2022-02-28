762955a83043b9e38e7fa96e361219542dcd15fa
wgzhao <wgzhao@gmail.com>
2022-02-28 21:39:10 +0800

    [tdengine-reader] Refactor code references official repo (#536)

--- plugin/reader/tdenginereader/src/main/java/com/wgzhao/addax/plugin/reader/tdenginereader/TDengineReader.java
+++ plugin/reader/tdenginereader/src/main/java/com/wgzhao/addax/plugin/reader/tdenginereader/TDengineReader.java
@@ -19,14 +19,22 @@
 
 package com.wgzhao.addax.plugin.reader.tdenginereader;
 
+import com.wgzhao.addax.common.exception.AddaxException;
 import com.wgzhao.addax.common.plugin.RecordSender;
 import com.wgzhao.addax.common.spi.Reader;
 import com.wgzhao.addax.common.util.Configuration;
 import com.wgzhao.addax.rdbms.reader.CommonRdbmsReader;
 import com.wgzhao.addax.rdbms.util.DataBaseType;
+import org.apache.commons.lang3.StringUtils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import static com.wgzhao.addax.common.base.Constant.DEFAULT_DATE_FORMAT;
 import static com.wgzhao.addax.common.base.Constant.DEFAULT_FETCH_SIZE;
 import static com.wgzhao.addax.common.base.Key.FETCH_SIZE;
 
@@ -56,12 +64,78 @@ public class TDengineReader
         public void preCheck()
         {
             this.commonRdbmsReaderJob.preCheck(this.originalConfig, DATABASE_TYPE);
+
+            SimpleDateFormat format = new SimpleDateFormat(DEFAULT_DATE_FORMAT);
+            // check beginDateTime
+            String beginDatetime = this.originalConfig.getString(TDKey.BEGIN_DATETIME);
+            if (StringUtils.isBlank(beginDatetime)) {
+                throw AddaxException.asAddaxException(TDengineReaderErrorCode.REQUIRED_VALUE, "The parameter [" + TDKey.BEGIN_DATETIME + "] is not set.");
+            }
+            long start;
+            try {
+                start = format.parse(beginDatetime).getTime();
+            }
+            catch (ParseException e) {
+                throw AddaxException.asAddaxException(TDengineReaderErrorCode.ILLEGAL_VALUE, "The parameter [" + TDKey.BEGIN_DATETIME +
+                        "] needs to conform to the [" + DEFAULT_DATE_FORMAT + "] format.");
+            }
+
+            // check endDateTime
+            String endDatetime = this.originalConfig.getString(TDKey.END_DATETIME);
+            if (StringUtils.isBlank(endDatetime)) {
+                throw AddaxException.asAddaxException(TDengineReaderErrorCode.REQUIRED_VALUE, "The parameter [" + TDKey.END_DATETIME + "] is not set.");
+            }
+            long end;
+            try {
+                end = format.parse(endDatetime).getTime();
+            }
+            catch (ParseException e) {
+                throw AddaxException.asAddaxException(TDengineReaderErrorCode.ILLEGAL_VALUE, "The parameter [" + TDKey.END_DATETIME + "] " +
+                        "needs to conform to the [" + DEFAULT_DATE_FORMAT + "] format.");
+            }
+            if (start >= end) {
+                throw AddaxException.asAddaxException(TDengineReaderErrorCode.ILLEGAL_VALUE, "The parameter [" + TDKey.BEGIN_DATETIME +
+                        "] should be less than the parameter [" + TDKey.END_DATETIME + "].");
+            }
+
+            // check splitInterval
+            String splitInterval = this.originalConfig.getString(TDKey.SPLIT_INTERVAL);
+            Long split;
+            if (StringUtils.isBlank(splitInterval)) {
+                throw AddaxException.asAddaxException(TDengineReaderErrorCode.REQUIRED_VALUE, "The parameter [" + TDKey.SPLIT_INTERVAL +
+                        "] is not set.");
+            }
+            try {
+                split = parseSplitInterval(splitInterval);
+            }
+            catch (Exception e) {
+                throw AddaxException.asAddaxException(TDengineReaderErrorCode.ILLEGAL_VALUE, "The parameter [" + TDKey.SPLIT_INTERVAL +
+                        "] should be like: \"123d|h|m|s\", error: " + e.getMessage());
+            }
+
+            this.originalConfig.set(TDKey.BEGIN_DATETIME, start);
+            this.originalConfig.set(TDKey.END_DATETIME, end);
+            this.originalConfig.set(TDKey.SPLIT_INTERVAL, split);
         }
 
         @Override
         public List<Configuration> split(int adviceNumber)
         {
-            return this.commonRdbmsReaderJob.split(this.originalConfig, adviceNumber);
+            List<Configuration> configurations = new ArrayList<>();
+            // do split
+            Long start = this.originalConfig.getLong(TDKey.BEGIN_DATETIME);
+            Long end = this.originalConfig.getLong(TDKey.END_DATETIME);
+            Long split = this.originalConfig.getLong(TDKey.SPLIT_INTERVAL);
+            
+            for (Long ts = start; ts < end; ts += split) {
+                Configuration clone = this.originalConfig.clone();
+                clone.remove(TDKey.SPLIT_INTERVAL);
+
+                clone.set(TDKey.BEGIN_DATETIME, ts);
+                clone.set(TDKey.END_DATETIME, Math.min(ts + split, end));
+                configurations.add(clone);
+            }
+            return configurations;
         }
 
         @Override
@@ -111,5 +185,36 @@ public class TDengineReader
         {
             this.commonRdbmsReaderTask.destroy(this.readerSliceConfig);
         }
+    }
+
+    private static final long second = 1000;
+    private static final long minute = 60 * second;
+    private static final long hour = 60 * minute;
+    private static final long day = 24 * hour;
+
+    private static Long parseSplitInterval(String splitInterval)
+            throws Exception
+    {
+        Pattern compile = Pattern.compile("^(\\d+)([dhms])$");
+        Matcher matcher = compile.matcher(splitInterval);
+        while (matcher.find()) {
+            Long value = Long.valueOf(matcher.group(1));
+            if (value == 0) {
+                throw new Exception("invalid splitInterval: 0");
+            }
+            char unit = matcher.group(2).charAt(0);
+            switch (unit) {
+                case 'd':
+                    return value * day;
+                default:
+                case 'h':
+                    return value * hour;
+                case 'm':
+                    return value * minute;
+                case 's':
+                    return value * second;
+            }
+        }
+        throw new Exception("invalid splitInterval: " + splitInterval);
     }
 }
