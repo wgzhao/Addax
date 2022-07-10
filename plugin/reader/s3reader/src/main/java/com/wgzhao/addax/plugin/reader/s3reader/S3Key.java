f6320b1095ac6c2f1183215d99e9458e79b49435
wgzhao <wgzhao@gmail.com>
2022-07-10 08:33:42 +0800

    Add support for AWS S3 reader (#613)
    Add support for AWS S3 reader #610

---
+++ plugin/reader/s3reader/src/main/java/com/wgzhao/addax/plugin/reader/s3reader/S3Key.java
@@ -0,0 +1,18 @@
+package com.wgzhao.addax.plugin.reader.s3reader;
+
+import com.wgzhao.addax.common.base.Key;
+
+public class S3Key extends Key
+{
+    public static final String REGION = "region";
+
+    public static final String ENDPOINT = "endpoint";
+
+    public static final String ACCESS_ID = "accessId";
+
+    public static final String ACCESS_KEY = "accessKey";
+
+    public static final String BUCKET = "bucket";
+
+    public static final String OBJECT = "object";
+}
---
+++ plugin/reader/s3reader/src/main/java/com/wgzhao/addax/plugin/reader/s3reader/S3ReaderErrorCode.java
@@ -0,0 +1,46 @@
+package com.wgzhao.addax.plugin.reader.s3reader;
+
+import com.wgzhao.addax.common.spi.ErrorCode;
+import org.omg.CORBA.OBJECT_NOT_EXIST;
+
+public enum S3ReaderErrorCode
+        implements ErrorCode
+{
+    S3_EXCEPTION("S3FileReader-01", "Exception occurred when reading configure"),
+    CONFIG_INVALID_EXCEPTION("S3FileReader-02", "Invalid configure"),
+    NOT_SUPPORT_TYPE("S3Reader-03", "Non-supported type"),
+    SECURITY_EXCEPTION("S3Reader-05", "Permission denied"),
+    ILLEGAL_VALUE("S3Reader-06", "Illegal value"),
+    REQUIRED_VALUE("S3Reader-07", "Missing required value"),
+    NO_INDEX_VALUE("S3Reader-08", "Missing index"),
+    MIXED_INDEX_VALUE("S3Reader-09", "Mix index and value"),
+    EMPTY_BUCKET_EXCEPTION("S3Reader-10", "Empty bucket"),
+    OBJECT_NOT_EXIST("S3Reader-11", "Object does not exists");
+
+    private final String code;
+    private final String description;
+
+    S3ReaderErrorCode(String code, String description)
+    {
+        this.code = code;
+        this.description = description;
+    }
+
+    @Override
+    public String getCode()
+    {
+        return this.code;
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("Code:[%s], Description:[%s].", this.code, this.description);
+    }
+}
