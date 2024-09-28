92a47bead0a2e4fe712b34d2edf5597f751a05ab
wgzhao <wgzhao@gmail.com>
2024-09-28 18:49:30 +0800

    [update][script] Add support for remote deployment using

--- build-module.sh
+++ build-module.sh
@@ -5,7 +5,7 @@ mvn_opts="-Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dmaven.source.skip=t
 function build_base() {
     cd $SRC_DIR
     mvn package -B $mvn_opts -pl :addax-core,:addax-rdbms,:addax-storage,:addax-transformer -am || exit 1
-    cp -a core/target/addax/* ${ADDAX_HOME}
+    rsync -a core/target/addax/* ${ADDAX_HOME}
     rsync -azv lib/addax-{rdbms,storage,transformer}/target/addax/lib/* ${ADDAX_HOME}/lib/
 }
 
@@ -15,16 +15,22 @@ fi
 
 if [ -z "$ADDAX_HOME" ]; then
    ADDAX_HOME=/opt/app/addax
-fi 
+fi
 
 if [ -z $1 ]; then
     echo "Usage: $0 module_name"
     exit 1
 fi
 
-[ -d $ADDAX_HOME ] || mkdir -p $ADDAX_HOME || exit 1
 
-[ -d $ADDAX_HOME/bin ] || build_base
+
+if [ -n "${REMOTE_HOST}" ]; then
+    echo "The building module will upload to ${REMOTE_HOST}"
+    ADDAX_HOME=${REMOTE_HOST}:${ADDAX_HOME}
+else
+    [ -d $ADDAX_HOME ] || mkdir -p $ADDAX_HOME || exit 1
+    [ -d $ADDAX_HOME/bin ] || build_base
+fi
 
 MODULE_NAME=$1
 
@@ -32,17 +38,17 @@ cd $SRC_DIR
 mvn package -B $mvn_opts -pl :$MODULE_NAME -am  || exit 1
 
 if [ "$MODULE_NAME" == "addax-core" ]; then
-    cp -a core/target/${MODULE_NAME}-${version}.jar ${ADDAX_HOME}/lib
+    scp  core/target/${MODULE_NAME}-${version}.jar ${ADDAX_HOME}/lib
     exit 0
 fi
 
 if [ "$MODULE_NAME" == "addax-common" ];then
-    cp -a common/target/${MODULE_NAME}-${version}.jar ${ADDAX_HOME}/lib
+    scp  common/target/${MODULE_NAME}-${version}.jar ${ADDAX_HOME}/lib
     exit 0
 fi
 
 if [ "$MODULE_NAME" == "addax-rdbms"  -o "$MODULE_NAME" == "addax-storage" -o "$MODULE_NAME" == "addax-transformer" ]; then
-    cp -a lib/${MODULE_NAME}/target/${MODULE_NAME}-${version}.jar ${ADDAX_HOME}/lib
+    scp  lib/${MODULE_NAME}/target/${MODULE_NAME}-${version}.jar ${ADDAX_HOME}/lib
     exit 0
 fi
 # if the module nam ends with reader, then the module base directory is plugin/reader,
@@ -57,5 +63,5 @@ else
 fi
 [ -d $ADDAX_HOME/$MODULE_DIR ] || mkdir -p $ADDAX_HOME/$MODULE_DIR || exit 1
 
-cp -a $MODULE_DIR/$MODULE_NAME/target/${MODULE_NAME}-${version}/$MODULE_DIR/${MODULE_NAME} \
+scp  $MODULE_DIR/$MODULE_NAME/target/${MODULE_NAME}-${version}/$MODULE_DIR/${MODULE_NAME} \
        	$ADDAX_HOME/$MODULE_DIR
