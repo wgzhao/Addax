da40cd9c2d8bc9cf7c62032f258a9d5b54757e86
awol2005ex <awol2005ex@163.com>
2025-06-20 13:49:22 +0800

    merge master

--- plugin/writer/icebergwriter/src/main/java/com/wgzhao/addax/plugin/writer/icebergwriter/IcebergWriter.java
+++ plugin/writer/icebergwriter/src/main/java/com/wgzhao/addax/plugin/writer/icebergwriter/IcebergWriter.java
@@ -332,7 +332,6 @@ public class IcebergWriter
                 DataWriter<GenericRecord> dataWriter = null;
 
                 if ("parquet".equals(fileFormat)) {
-                    log.info("start writing fileFormat: {}", fileFormat);
                     try {
                         dataWriter = Parquet.writeData(file).overwrite().forTable(table).createWriterFunc(GenericParquetWriter::buildWriter).build();
                     }
@@ -342,7 +341,6 @@ public class IcebergWriter
                 }
                 else if ("orc".equals(fileFormat)) {
 
-                    log.info("start writing fileFormat: {}", fileFormat);
                     try {
                         dataWriter = ORC.writeData(file).overwrite().forTable(table).createWriterFunc(GenericOrcWriter::buildWriter).build();
                     }
