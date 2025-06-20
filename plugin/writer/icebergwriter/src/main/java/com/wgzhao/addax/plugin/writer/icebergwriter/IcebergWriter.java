b5ab2ac371bfd6a4aa1007519e201cfd155c8870
awol2005ex <awol2005ex@163.com>
2025-06-20 13:40:31 +0800

    [bugfix][plugin][icebergwriter] fix error when write orc format table

--- plugin/writer/icebergwriter/src/main/java/com/wgzhao/addax/plugin/writer/icebergwriter/IcebergWriter.java
+++ plugin/writer/icebergwriter/src/main/java/com/wgzhao/addax/plugin/writer/icebergwriter/IcebergWriter.java
@@ -59,7 +59,12 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 public class IcebergWriter
         extends Writer
